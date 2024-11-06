1e811bcf3658db0b38d7cfef6f40b575cb87b533
killthecreep <121694020+killthecreep@users.noreply.github.com>
2024-11-05 20:26:58 -0700

    Update on Assignments
    Finished ica10

--- js/ica10.js
+++ js/ica10.js
@@ -1,27 +1,51 @@
-let my_var = 100;
-var my_other_var = "Boo!!!!";
-const this_is_a_constant = "constant"
-console.log(my_other_var);
-my_other_var += "!"
-// alert(my_other_var);
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
 
-// How to interact with the DOM
-// Document Object Model
-console.log(document.getElementById("my_head"));
-document.getElementById("my_head").addEventListener("click", function(e){
-    alert("booo!!!");
-    document.getElementById("paragraph").innerHTML = "This is the new text";
-    document.getElementById("paragraph").style.color = "blue";
-    document.getElementById("image").src = "../img/spongebob.jpg";
-})
+let names = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let locations = ["the soup kitchen", "Disneyland", "the White House"];
+let actions = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-console.log(document.getElementById("button"));
-document.getElementById("button").addEventListener("click", function(e){
-    alert("testing");
-    document.getElementById('paragraph').removeAttribute('paragraph');
-    document.getElementById("paragraph").style.color = "black";
-    document.getElementById("image").src = "../img/halloween pumpkins.jpg";
-})
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
 
-console.log(document.getElementById("my_head").innerHTML);
-console.log(document.getElementById("button").innerHTML);
+randomize.addEventListener('click', result);
+
+function result() {
+
+    console.log("testing");
+
+    if(customName.value !== '') {
+    const name = customName.value;
+    }
+
+    let newStory = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+    if(document.getElementById("uk").checked) {
+        const weight = Math.round(300 / 14);
+        const temperature =  Math.round((94 - 32) * (5/9));
+        newStory = newStory.replace("300 pounds", weight + ' stone');
+        newStory = newStory.replace("94 fahrenheit", temperature + ' centigrade');
+    }
+
+    newStory = newStory.replace(/:insertx:/g, randomValueFromArray(names));
+    newStory = newStory.replace(':inserty:', randomValueFromArray(locations));
+    newStory = newStory.replace(':insertz:', randomValueFromArray(actions));
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+
+    
+}
+
+
+
+// console.log(document.getElementById("my_head"));
+// document.getElementById("my_head").addEventListener("click", function(e){
+//     alert("booo!!!");
+//     document.getElementById("paragraph").innerHTML = "This is the new text";
+//     document.getElementById("paragraph").style.color = "blue";
+//     document.getElementById("image").src = "../img/spongebob.jpg";
+// })
